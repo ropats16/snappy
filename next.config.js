cc1567c90f312ea61c20127bbeb988b13bfef8bb
ropats16 <rohitcpathare@gmail.com>
2025-01-07 22:43:26 +0530

    update next config

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Enable static exports
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  // Since this is a static export, we don't need a basePath
+  // but if you deploy to a subfolder, you'll need to set it
+  // basePath: '/snappy-cam',
+};
+
+module.exports = nextConfig;
